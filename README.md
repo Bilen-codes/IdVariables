3b6718c522ab322bdf1f303aed55062909f7b623
Bilen Worku <bilenworku@Bilens-Air.fios-router.home>
2020-04-15 10:21:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Idntification of Variables
---
+++ src/IdVariables.java
@@ -0,0 +1,20 @@
+/*This program will display a message out of initialized strings  */
+
+public class IdVariables {
+    public static void main(String []args){
+        String country = "Ethiopia";
+        int ppl = 1000000000;
+        String capital = "Addis Abeba";
+        int money = 40;
+        System.out.println("Hey mon and Dad! \n Identifying the variables\n");
+        System.out.println("Name: "+ country);
+        System.out.println("Population size " + ppl);
+        System.out.println("capital: " + capital);
+        System.out.println("Currency: " + money);
+        System.out.println("I'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later. ");
+
+
+
+
+    }
+}
